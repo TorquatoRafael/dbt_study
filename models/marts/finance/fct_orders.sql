61bfdc0fa1a1653788199d649e123fc711b662b4
Rafael Torquato <rafastorquato@hotmail.com>
2026-03-10 21:17:26 -0300

    Aula de configuração de modelos incrementais

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,3 +1,12 @@
+{{ 
+    config(
+        materialized='incremental' ,
+        unique_key = 'order_id' ,
+        incremental_strategy = 'merge' 
+        )
+    }}
+
+
 with orders as (
     select * from {{ ref('stg_jaffle_shop__orders') }}
 ),
@@ -11,7 +20,7 @@ order_payments as (
         order_id
         , sum(case when status = 'success' then amount end) as amount
     from payments
-    group by 1
+    group by order_id
 ),
 
 final as (
@@ -25,3 +34,7 @@ final as (
 )
 
 select * from final
+{% if is_incremental() %}
+where 
+order_date >= (select max(order_date) from {{ this }})
+{% endif %}
